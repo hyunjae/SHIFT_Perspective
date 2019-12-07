ca9995bfb4b274da329e85c157f50bdff472db1c
Gabriel Lee <gabriel@timesaved.ca>
2019-12-06 23:54:21 -0500

    converted react from class to functional components also added some configuration changes

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,26 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
+import ReactDOM from "react-dom";
 import { BrowserRouter, Route, Switch } from "react-router-dom";
+import QuestionPage from "./QuestionPage";
 
-export default class App extends Component {
-    render() {
-        return (
-            <div className="container">
-                <div className="row justify-content-center">
-                    <div className="col-md-8">
-                        <div className="card">
-                            <div className="card-header">Example Component</div>
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={QuestionPage} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
-                            <div className="card-body">
-                                I'm an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        );
-    }
-}
+export default App;
 
 if (document.getElementById("app")) {
-    ReactDOM.render(<App />, document.getElementById("app"));
+  ReactDOM.render(<App />, document.getElementById("app"));
 }
---
+++ resources/js/components/QuestionPage.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const QuestionPage = props => {
+  return (
+    <div>
+      <p>List of Questions!</p>
+    </div>
+  );
+};
+
+export default QuestionPage;
